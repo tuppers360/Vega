5cd89853764bbec80e40856b0361636e8289d8ba
Gareth Tupman <tuppers360@live.co.uk>
2017-04-20 17:00:12 +0100

    Create Vehicle API endpoint - not returning correct result

--- Controllers/VehiclesController.cs
+++ Controllers/VehiclesController.cs
@@ -1,15 +1,36 @@
+using System;
+using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using vega.Controllers.Resources;
 using vega.Models;
+using vega.Persistence;
 
 namespace vega.Controllers
 {
     [Route("/api/vehicles")]
     public class VehiclesController : Controller
     {
-        [HttpPost]
-        public IActionResult CreateVehicle([FromBody] Vehicle vehicle)
+        private readonly IMapper mapper;
+        private readonly VegaDbContext context;
+        public VehiclesController(IMapper mapper, VegaDbContext context)
         {
-            return Ok(vehicle);
+            this.context = context;
+            this.mapper = mapper;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateVehicle([FromBody] VehicleResource vehicleResource)
+        {
+            var vehicle = mapper.Map<VehicleResource, Vehicle>(vehicleResource);
+            vehicle.LastUpdate = DateTime.Now;
+            
+            context.Vehicles.Add(vehicle);
+            await context.SaveChangesAsync();
+
+            var result = mapper.Map<Vehicle, VehicleResource>(vehicle);
+            
+            return Ok(result);
         }
     }
 }
--- Persistance/VegaDbContext.cs
+++ Persistance/VegaDbContext.cs
@@ -5,9 +5,9 @@ namespace vega.Persistence
 {
     public class VegaDbContext : DbContext
     {
-
-        public DbSet<Make> Makes { get; set; }
         public DbSet<Feature> Features { get; set; }
+        public DbSet<Make> Makes { get; set; }
+        public DbSet<Vehicle> Vehicles { get; set; }
 
         public VegaDbContext(DbContextOptions<VegaDbContext> options) : base(options) { }
 
