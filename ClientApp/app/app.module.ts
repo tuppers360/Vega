460d23d44e66ce2ac6021ef9a626e8a319c4c4bf
Gareth Tupman <tuppers360@live.co.uk>
2017-05-06 16:36:53 +0100

    Revert "Populate submit form"

--- ClientApp/app/app.module.ts
+++ ClientApp/app/app.module.ts
@@ -1,10 +1,6 @@
-import * as Raven from 'raven-js';
-import { AppErrorHandler } from './app.error-handler';
-import { ErrorHandler } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { ToastyModule } from 'ng2-toasty';
 import { UniversalModule } from 'angular2-universal';
 
 import { AppComponent } from './components/app/app.component'
@@ -14,8 +10,6 @@ import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
 import { CounterComponent } from './components/counter/counter.component';
 import { VehicleFormComponent } from './components/vehicle-form/vehicle-form.component';
 import { VehicleService } from './services/vehicle.service';
-
-Raven.config('https://6e3cdac5e0cc4ac6bb2871c887764584@sentry.io/165296').install();
 
 @NgModule({
     bootstrap: [AppComponent],
@@ -28,21 +22,18 @@ Raven.config('https://6e3cdac5e0cc4ac6bb2871c887764584@sentry.io/165296').instal
         VehicleFormComponent
     ],
     imports: [
-        UniversalModule, // Must be first import. This automatically imports BrowserModule, HttpModule, and JsonpModule too.
         FormsModule,
-        ToastyModule.forRoot(),
+        UniversalModule, // Must be first import. This automatically imports BrowserModule, HttpModule, and JsonpModule too.
         RouterModule.forRoot([
             { path: '', redirectTo: 'home', pathMatch: 'full' },
             { path: 'home', component: HomeComponent },
             { path: 'vehicles/new', component: VehicleFormComponent },
-            { path: 'vehicles/:id', component: VehicleFormComponent },
             { path: 'counter', component: CounterComponent },
             { path: 'fetch-data', component: FetchDataComponent },
             { path: '**', redirectTo: 'home' }
         ])
     ],
     providers: [
-        { provide: ErrorHandler, useClass: AppErrorHandler },
         VehicleService
     ]
 })
--- webpack.config.vendor.js
+++ webpack.config.vendor.js
@@ -31,11 +31,7 @@ module.exports = (env) => {
                 'es6-shim',
                 'es6-promise',
                 'event-source-polyfill',
-                'ng2-toasty',
-                'ng2-toasty/bundles/style-bootstrap.css',
                 'jquery',
-                'raven-js',
-                'underscore',
                 'zone.js',
             ]
         },
