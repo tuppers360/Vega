84260a8d53945e9d488495af55770e09b208a8a7
Gareth Tupman <tuppers360@live.co.uk>
2017-05-06 18:33:39 +0100

    Revert "Revert "Populate submit form""

--- ClientApp/app/app.module.ts
+++ ClientApp/app/app.module.ts
@@ -1,6 +1,10 @@
+import * as Raven from 'raven-js';
+import { AppErrorHandler } from './app.error-handler';
+import { ErrorHandler } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { ToastyModule } from 'ng2-toasty';
 import { UniversalModule } from 'angular2-universal';
 
 import { AppComponent } from './components/app/app.component'
@@ -10,6 +14,8 @@ import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
 import { CounterComponent } from './components/counter/counter.component';
 import { VehicleFormComponent } from './components/vehicle-form/vehicle-form.component';
 import { VehicleService } from './services/vehicle.service';
+
+Raven.config('https://6e3cdac5e0cc4ac6bb2871c887764584@sentry.io/165296').install();
 
 @NgModule({
     bootstrap: [AppComponent],
@@ -22,18 +28,21 @@ import { VehicleService } from './services/vehicle.service';
         VehicleFormComponent
     ],
     imports: [
-        FormsModule,
         UniversalModule, // Must be first import. This automatically imports BrowserModule, HttpModule, and JsonpModule too.
+        FormsModule,
+        ToastyModule.forRoot(),
         RouterModule.forRoot([
             { path: '', redirectTo: 'home', pathMatch: 'full' },
             { path: 'home', component: HomeComponent },
             { path: 'vehicles/new', component: VehicleFormComponent },
+            { path: 'vehicles/:id', component: VehicleFormComponent },
             { path: 'counter', component: CounterComponent },
             { path: 'fetch-data', component: FetchDataComponent },
             { path: '**', redirectTo: 'home' }
         ])
     ],
     providers: [
+        { provide: ErrorHandler, useClass: AppErrorHandler },
         VehicleService
     ]
 })
---
+++ ClientApp/app/models/vehilce.ts
@@ -0,0 +1,12 @@
+import { KeyValuePair } from "./keyvaluepair";
+import { Contact } from "./contact";
+
+export interface Vehicle {
+    id: number;
+    model: KeyValuePair;
+    make: KeyValuePair;
+    isRegistered: boolean;
+    features: KeyValuePair[];
+    contact: Contact;
+    lastUpdate: string;
+}
