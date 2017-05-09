f8548ada2d0468fdc885f9dadace312dbc880d70
Gareth Tupman <tuppers360@live.co.uk>
2017-05-09 10:43:27 +0100

    implement client side filtering

--- ClientApp/app/components/vehicle-list/vehicle-list.component.ts
+++ ClientApp/app/components/vehicle-list/vehicle-list.component.ts
@@ -1,3 +1,4 @@
+import { KeyValuePair } from './../../models/keyvaluepair';
 import { VehicleService } from './../../services/vehicle.service';
 import { Vehicle } from './../../models/vehilce';
 import { Component, OnInit } from '@angular/core';
@@ -7,14 +8,37 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './vehicle-list.component.html',
   styleUrls: ['./vehicle-list.component.css']
 })
+
 export class VehicleListComponent implements OnInit {
   vehicles: Vehicle[];
+  allVehicles: Vehicle[];
+  makes: KeyValuePair[];
+  filter: any = {};
 
   constructor(private vehicleService: VehicleService) { }
 
   ngOnInit() {
+    this.vehicleService.getMakes()
+      .subscribe(makes => this.makes = makes);
+
     this.vehicleService.getVehicles()
-      .subscribe(vehicles => this.vehicles = vehicles);
+      .subscribe(vehicles => this.vehicles = this.allVehicles = vehicles);
   }
 
+  onFilterChange() {
+    var vehicles = this.allVehicles;
+
+    if (this.filter.makeId)
+      vehicles = vehicles.filter(v => v.make.id == this.filter.makeId);
+
+    if (this.filter.modelId)
+      vehicles = vehicles.filter(v => v.model.id == this.filter.modelId);
+
+    this.vehicles = vehicles;
+  }
+
+  resetFilter() {
+    this.filter = {};
+    this.onFilterChange();
+  }
 }
