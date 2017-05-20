385f5ff552769c991f45ed114ac7b14c3f3cacc6
Gareth Tupman <tuppers360@live.co.uk>
2017-05-20 21:17:05 +0100

    extend domain to add photos

---
+++ Migrations/20170520201533_AddPhoto.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace vega.Migrations
+{
+    public partial class AddPhoto : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Models_Makes_MakeId",
+                table: "Models");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "MakeId",
+                table: "Models",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldNullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Photos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    FileName = table.Column<string>(maxLength: 255, nullable: false),
+                    VehicleId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Photos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Photos_Vehicles_VehicleId",
+                        column: x => x.VehicleId,
+                        principalTable: "Vehicles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Photos_VehicleId",
+                table: "Photos",
+                column: "VehicleId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Models_Makes_MakeId",
+                table: "Models",
+                column: "MakeId",
+                principalTable: "Makes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Models_Makes_MakeId",
+                table: "Models");
+
+            migrationBuilder.DropTable(
+                name: "Photos");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "MakeId",
+                table: "Models",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Models_Makes_MakeId",
+                table: "Models",
+                column: "MakeId",
+                principalTable: "Makes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
